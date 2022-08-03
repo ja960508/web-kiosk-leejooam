27454b9e67af7bc5cc5a94d81ba92dc2cf11da0b
jooam <lord0325@naver.com>
2022-08-04 01:19:01 +0900

    feat: TypeORM 초기설정, validationPipe 설

--- backend/src/config/validationScheme.ts
+++ backend/src/config/validationScheme.ts
@@ -2,6 +2,11 @@ import * as Joi from 'joi';
 
 const validationSchema = Joi.object({
   PORT: Joi.number().required(),
+  DATABASE_USERNAME: Joi.string().required(),
+  DATABASE_PASSWORD: Joi.string().required(),
+  DATABASE_PORT: Joi.string().required(),
+  DATABASE_HOST: Joi.string().required(),
+  DATABASE_DATABASE: Joi.string().required(),
 });
 
 export default validationSchema;
