2fd8220ceef5a027772908ac8394071cbeeba7d2
jooam <lord0325@naver.com>
2022-08-06 17:41:31 +0900

    refactor: 기존 TypeORM code 삭제

--- backend/src/product/dto/update-product.dto.ts
+++
@@ -1,6 +0,0 @@
-import { OmitType, PartialType } from '@nestjs/mapped-types';
-import { CreateProductDto } from './create-product.dto';
-
-export class UpdateProductDto extends PartialType(
-  OmitType(CreateProductDto, ['category']),
-) {}
