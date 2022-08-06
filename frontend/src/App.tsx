0e65fa3bdc4835fab8981db4889f8fcf68f7bee8
jooam <lord0325@naver.com>
2022-08-06 18:28:05 +0900

    feat: 초기 세팅용 globalStyle, pages 생성

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
+import Route from './lib/Router/Route';
+import Routes from './lib/Router/Routes';
+import AdminStore from './pages/AdminStore';
+import CustomerOrder from './pages/CustomerOrder';
+import Entrance from './pages/Entrance';
+import GlobalStyle from './styles/global';
+
+const Wrapper = styled.div`
+  position: relative;
+  max-width: 1080px;
+  margin: 0 auto;
+`;
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <GlobalStyle />
+      <Wrapper>
+        <Routes>
+          <Route path="/admin" element={<AdminStore />} />
+          <Route path="/customer" element={<CustomerOrder />} />
+          <Route path="/" element={<Entrance />} />
+        </Routes>
+      </Wrapper>
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/styles/reset.ts
@@ -0,0 +1,16 @@
+import { css } from 'styled-components';
+
+const reset = css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`;
+
+export default reset;
