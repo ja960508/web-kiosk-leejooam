5f61d82cb4af0794201b2d03a9c5b2e35ec83e96
jooam <lord0325@naver.com>
2022-08-08 01:02:56 +0900

    chore: 라우터 테스트용 페이지 컴포넌트 제거

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
 import AdminStore from './pages/AdminStore';
 import CustomerOrder from './pages/CustomerOrder';
 import Entrance from './pages/Entrance/Entrance';
-import List from './pages/List';
-import ListId from './pages/ListId';
-import ListIdId from './pages/ListIdId';
 import GlobalStyle from './styles/global';
 import { Routes, Route } from './lib/Router';
 
@@ -16,9 +12,6 @@ function App() {
       <Routes>
         <Route path="/admin" element={<AdminStore />} />
         <Route path="/customer" element={<CustomerOrder />} />
-        <Route path="/list" element={<List />} />
-        <Route path="/list/:id" element={<ListId />} />
-        <Route path="/list/:id/:toId" element={<ListIdId />} />
         <Route path="/" element={<Entrance />} />
       </Routes>
     </>
