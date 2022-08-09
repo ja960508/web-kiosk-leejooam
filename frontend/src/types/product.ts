a20d0ce190ed54084786b446bda07334b63cd18b
jooam <lord0325@naver.com>
2022-08-09 09:48:20 +0900

    feat: ProductType 타입 추가작업

--- frontend/src/types/product.ts
+++ frontend/src/types/product.ts
@@ -1,9 +1,13 @@
+import { ProductOptionType } from '../components/ProductList/ProductList';
+
 export interface ProductType {
   id: number;
   name: string;
   thumbnail: string;
   price: string;
   categoryId: number;
+  isPopular: boolean;
+  isSoldOut: boolean;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  productOption: any;
+  productOption: ProductOptionType[];
 }
