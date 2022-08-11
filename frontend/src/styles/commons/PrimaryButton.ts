c1b94fa1c1c4082608f6645da7d6630fbfb6f596
jooam <lord0325@naver.com>
2022-08-11 20:51:54 +0900

    design: 상품 추가하기 버튼 스타일링
    - Primary 버튼 공통 스타일로 분리

---
+++ frontend/src/styles/commons/PrimaryButton.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import color from '../variables/color';
+import shadow from '../variables/shadow';
+
+export const PrimaryButton = styled.button`
+  background-color: ${color.red};
+  color: ${color.white};
+  padding: 1rem;
+  margin: 0.5rem 0;
+  border-radius: 10px;
+  box-shadow: ${shadow.normalShadow};
+  font-size: 1rem;
+  display: flex;
+  align-items: center;
+
+  svg {
+    width: 1.75rem;
+    height: 1.75rem;
+    margin-right: 0.25rem;
+  }
+`;
