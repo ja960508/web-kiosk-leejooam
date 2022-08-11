471db68328132194ea99087b4adc07f5a5373fa4
jooam <lord0325@naver.com>
2022-08-11 22:16:24 +0900

    design: 카테고리 추가 모달 스타일링

--- frontend/src/components/Modal/CategoryModal/CategoryAddModal.tsx
+++ frontend/src/components/Modal/CategoryModal/CategoryAddModal.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { CategoryType } from '../../../types/category';
 import categoryAPI from '../../../api/categoryAPI';
+import styled from 'styled-components';
+import shadow from '../../../styles/variables/shadow';
+import color from '../../../styles/variables/color';
 
 interface CategoryAddModalProps {
   storeId: number;
@@ -29,12 +32,41 @@ function CategoryAddModal({
   };
 
   return (
-    <form onSubmit={handleAddCategory}>
+    <StyledCategoryAddForm onSubmit={handleAddCategory}>
       <strong>추가할 카테고리 이름을 입력해주세요.</strong>
       <input type="text" name="categoryName" autoFocus autoComplete="off" />
       <button type="submit">추가</button>
-    </form>
+    </StyledCategoryAddForm>
   );
 }
 
 export default CategoryAddModal;
+
+const StyledCategoryAddForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  padding: 2.5rem;
+  box-shadow: ${shadow.normalShadow};
+
+  strong {
+    font-size: 1.25rem;
+    margin-bottom: 1.5rem;
+  }
+
+  input {
+    width: 20rem;
+    padding: 10px 20px;
+    border: 1px solid ${color.gray};
+    border-radius: 10px;
+    margin-bottom: 1rem;
+    font-size: 1rem;
+  }
+
+  button[type='submit'] {
+    padding: 0.75rem;
+    border-radius: 10px;
+    color: ${color.white};
+    font-size: 1rem;
+    background-color: ${color.maroon};
+  }
+`;
--- frontend/src/styles/commons/DeleteModalForm.ts
+++ frontend/src/styles/commons/DeleteModalForm.ts
@@ -4,6 +4,10 @@ import shadow from '../variables/shadow';
 
 export const StyledDeleteModalForm = styled.form`
   box-shadow: ${shadow.normalShadow};
+  display: flex;
+  flex-direction: column;
+  padding: 2.5rem;
+  border-radius: 10px;
 
   strong {
     font-size: 1.25rem;
